db1cf011af80d58eeff59b7eb65ab45f7db816b7
georgedouzas <georgios.douzas@gmail.com>
2019-06-14 01:47:44 +0300

    Added all loaders and jquery plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -10,10 +11,14 @@ module.exports = {
     },
     module: {
         rules: [
-            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
+            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+            {test: /\.(png|svg|jpg|gif)$/, use: 'file-loader'},
+            {test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader'},
+            { test: /\.html$/, use: 'html-loader' }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({title: 'AlgoWit', template: './src/index.html'})
+        new HtmlWebpackPlugin({template: './src/index.html', favicon: './src/assets/static/favicon.png'}),
+        new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'})
       ]
 };
