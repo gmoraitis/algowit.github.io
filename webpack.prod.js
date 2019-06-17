5846dc9b4f11ad4f572c408cd300937a3d077125
gmoraitis <moraitis.georgios@icloud.com>
2019-06-17 17:17:54 +0300

    Added plugin for scss extraction

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -11,14 +12,16 @@ module.exports = {
     },
     module: {
         rules: [
-            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+            {test: /\.scss$/, use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']},
             {test: /\.(png|svg|jpg|gif)$/, use: 'file-loader'},
             {test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader'},
-            { test: /\.html$/, use: 'html-loader' }
+            { test: /\.html$/, use: 'html-loader' },
+            
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({template: './src/index.html', favicon: './src/assets/static/favicon.png'}),
-        new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'})
+        new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'}),
+        new MiniCssExtractPlugin({filename: "style.scss"})
       ]
 };
