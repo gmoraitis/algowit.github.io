0ee999462bab7027260031b512da185baa58198e
gmoraitis <moraitis.georgios@icloud.com>
2019-06-20 15:31:05 +0300

    Added plugin for minification and small changes in production mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
     mode: 'production',
     entry: './src/assets/scripts/script.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'), 
     },
-    module: {
-        rules: [
-            {test: /\.scss$/, use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']},
-            {test: /\.(png|svg|jpg|gif)$/, use: 'file-loader'},
-            {test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader'},
-            { test: /\.html$/, use: 'html-loader' },
-            
-        ]
-    },
+    optimization: {minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]},
     plugins: [
-        new HtmlWebpackPlugin({template: './src/index.html', favicon: './src/assets/static/favicon.png'}),
-        new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'}),
-        new MiniCssExtractPlugin({filename: "style.css"})
-      ]
+        new HtmlWebpackPlugin({title:'AlgoWit', template: './src/index.html', favicon: './src/assets/static/favicon.png', minify: {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true
+        }}),
+        new MiniCssExtractPlugin({filename: "style.css"}),
+        new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'}) 
+      ],
+      module: {
+        rules: [
+        {test: /\.(png|svg|jpg|gif)$/, use: 'file-loader'},
+        {test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader'},
+        { test: /\.html$/, use: 'html-loader' },
+        {test: /\.scss$/, use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']}, 
+        ]
+    }
+      
 };
