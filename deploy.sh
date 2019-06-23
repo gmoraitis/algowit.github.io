86d8d8fc49e6c42c032ab45a7d08cc0f96ad0d7e
georgedouzas <georgios.douzas@gmail.com>
2019-06-23 19:22:48 +0300

    Fixed again deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
-npm run build
-git checkout -B gh-pages
-shopt -s extglob
-rm -R -df !(build|node_modules)
-mv build/* .
-git add --all && \
-git commit -m $1 && \
-git push origin gh-pages --force && \
-git checkout master && \
-git branch -D gh-pages && \
-git reset --hard
+# Check commit name for deploy
+if [ -z "$1" ]
+  then
+    echo "No commit name is given."
+fi
+
+# Change to build directory and push gh-pages branch
+npm run build & \
+cd build & \
+git init & \
+git add --all & \
+git commit $1 & \
+git checkout -B gh-pages & \
+git push https://github.com/AlgoWit/algowit-website.git gh-pages --force 
