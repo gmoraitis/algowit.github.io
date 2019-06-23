c3726fa5f02bcb4b5dd53052ae2f0b4693aa156e
georgedouzas <georgios.douzas@gmail.com>
2019-06-23 19:45:47 +0300

    Finalized script

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ if [ -z "$1" ]
 fi
 
 # Change to build directory and push gh-pages branch
-npm run build & \
-cd build & \
-git init & \
-git add --all & \
-git commit $1 & \
-git checkout -B gh-pages & \
+npm run build && \
+cd build && \
+git init && \
+git add --all && \
+git commit -m "$1" && \
+git checkout -B gh-pages && \
 git push https://github.com/AlgoWit/algowit-website.git gh-pages --force 
