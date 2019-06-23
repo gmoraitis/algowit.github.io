3533d0f61adc71852439bcc330ec2cb44e4fa664
georgedouzas <georgios.douzas@gmail.com>
2019-06-23 18:19:42 +0300

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ git checkout -B gh-pages
 shopt -s extglob
 rm -R -df !(build|node_modules)
 mv build/* .
-git add --all && git commit -m $1
-git push origin gh-pages --force
-git checkout master
-git branch -D gh-pages
+git add --all && \
+git commit -m $1 && \
+git push origin gh-pages --force && \
+git checkout master && \
+git branch -D gh-pages && \
+git reset --hard
