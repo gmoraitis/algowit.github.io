41b1e59fd732b51c09562acba3aacb968c653749
gmoraitis <moraitis.georgios@icloud.com>
2019-08-12 19:14:01 +0300

    Changed navbar

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -2,11 +2,11 @@ import '../styles/style.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'jquery.easing/jquery.easing';
 
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -20,7 +20,7 @@ import 'jquery.easing/jquery.easing';
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -31,7 +31,7 @@ import 'jquery.easing/jquery.easing';
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
